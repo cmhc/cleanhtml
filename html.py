75d413be045eabbdbd5842806f7ffdd5768138eb
huchao <hu_chao@139.com>
2015-05-12 09:18:26 +0800

    添加清理标签功能，更新readme

--- html.py
+++ html.py
@@ -16,26 +16,37 @@ clean style
 import re
 
 
+#默认忽略参数
+
 #清理，参数为html内容
+def clean(html,ignore="img",deltags="a|span"):
     
-def clean(html):
-        
     #首先只留下标签
-    exp = "<(?P<tag>(?!img|a)\w*)\s[^>]*>"#只留下标签,但是保留a标签和img标签
+    if ignore == '':
+        exp = "<(?P<tag>\w*)\s[^>]*>"#忽略为空，清除所有的标记
+    else:
+        exp = "<(?P<tag>(?!"+ignore+")\w*)\s[^>]*>"#只留下标签,但是保留a标签和img标签
+        
     match =re.match(exp,html)
     sub = "<\g<tag>>"
     html = re.sub(exp,sub,html);
         
     #去除内有内容的标签,反向引用之前匹配到的tag
-    exp = "<(?P<tag>\w*)>\s{0,}</(?P=tag)>"
+    exp = "<(?P<tag>\w*)>\s{0,}</(?P=tag)[^>]>"
     sub = ''
     html = re.sub(exp,sub,html)
 
-    #清除脚本和样式表
+    #清除脚本和样式表以及空行
     exp = "<(?P<tag>script|style)>[^<]*</(?P=tag)>"
     sub = ''
     html = re.sub(exp,sub,html)
 
+    #删除a和span标签
+    if deltags != '':
+        exp = "</?(?:" +deltags+ ")?>"
+        sub = ''
+        html = re.sub(exp,sub,html)
+    
     #清除所有换行符
     html = html.replace("\n","")
     return html
@@ -43,4 +54,4 @@ def clean(html):
 '''
 进行测试
 '''
-#print clean("<strong sdfsd></strong><img src='' /><a href=''></a><html sdfs></htmkl>")
+#print clean("<strong sdfsd>he</strong><img src='' /><span>这是span标签</span><br /><a  href=''>这是a标签</a><html sdfs></html>","","")
